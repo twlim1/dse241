abda67815a4e9463961e197b59eebaaa0b42ef1e
Teck Lim <tlim@qti.qualcomm.com>
2021-02-28 20:29:38 -0800

    Draft: final

---
+++ final/final.py
@@ -0,0 +1,195 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+from dash.dependencies import Input, Output
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+tabs_styles = {
+    'height': '40px',
+    'width': '48%',
+}
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '6px',
+    'fontWeight': 'bold',
+}
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#119DFF',
+    'color': 'white',
+    'padding': '6px',
+}
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# Reading the data
+df = pd.read_csv(r'data/all.csv')
+country_list = df['Country'].sort_values().unique()
+year_max = df['Year'].max()
+year_min = df['Year'].min()
+
+# Basic Stats 
+print('Dataframe shape: {}'.format(df.shape))
+print('Attacks between: {} - {}'.format(df['Year'].min(), df['Year'].max()))
+
+app.layout = html.Div([
+    # DatePickerRange
+    html.Div(id='slider-text', children=[html.H3('Global Terrorist Attacks {}-{}'.format(year_min, year_max))]),
+    dcc.RangeSlider(
+        id='year-range-slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=[df['Year'].min(), df['Year'].max()],
+        marks={str(year): ('' if year % 2 else str(year)) for year in df['Year'].unique()},
+        step=1
+    ),
+    dcc.Tabs(children=[
+        # Tab 1
+        dcc.Tab(label='World Graph', children=[
+            # DatePickerRange
+            html.Br(),
+            dcc.Tabs(children=[
+                dcc.Tab(label='Both', style=tab_style, selected_style=tab_selected_style),
+                dcc.Tab(label='Killed', style=tab_style, selected_style=tab_selected_style),
+                dcc.Tab(label='Injuried', style=tab_style, selected_style=tab_selected_style)
+            ], style=tabs_styles),
+            html.H3('map here'),
+        ]),
+        # Tab 2
+        dcc.Tab(label='Dashboard', children=[
+            # Graph 1
+            dcc.Graph(id='graph_1'),
+            # Graph 2
+            dcc.Graph(id='graph_2')
+        ]),
+        # Tab 3
+        dcc.Tab(label='Exploratory', children=[
+            html.Div([
+                html.Div([
+                    html.H6('Country'),
+                    dcc.Dropdown(
+                        id='dropdown_1',
+                        options=[{'label': i, 'value': i} for i in country_list],
+                        value='United States'
+                        # value='Iraq'
+                    )],
+                    style={'width': '48%', 'display': 'inline-block'}),
+
+                html.Div([
+                    html.H6('Data'),
+                    dcc.Dropdown(
+                        id='dropdown_2',
+                        options=[{'label': i, 'value': i} for i in ('Attack Type', 'City', 'Weapon Type')],
+                        multi=True,
+                        value=['City']
+                    )],
+                    style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+            ]),
+            dcc.RadioItems(
+                id='map_type',
+                options=[{'label': i, 'value': i} for i in ['Both', 'Killed', 'Injured']],
+                value='Choropleth',
+                labelStyle={'display': 'inline-block'}
+            ),
+            # Graph 3
+            dcc.Graph(id='graph_3')
+        ])
+    ])
+])
+
+
+def update_graph_line(df_input):
+    # Create traces
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_input['Year'], y=df_input['Attacks'],
+                             mode='lines+markers', name='Attacks'))
+    fig.add_trace(go.Scatter(x=df_input['Year'], y=df_input['Killed'],
+                             mode='lines+markers', name='Killed'))
+    fig.add_trace(go.Scatter(x=df_input['Year'], y=df_input['Wounded'],
+                             mode='lines+markers', name='Wounded'))
+
+    # Edit the layout
+    fig.update_layout(title='Terrorist attacks YoY', xaxis_title='Year', yaxis_title='Count')
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 40, 'r': 40}, hovermode='closest')
+    return fig
+
+
+def update_range_slider_text(min_year, max_year):
+    return html.H3('Global Terrorist Attacks {}-{}'.format(min_year, max_year))
+
+
+def update_graph_scatter(df_input):
+    # Create traces
+    fig = px.scatter(df_input, x='Year', y='Attack', size='Killed', color='Attack Type', size_max=40)
+    return fig
+
+
+def update_graph_treemap(df_input, selections):
+    # Create traces
+    order = ['Country']
+    if selections:
+        for i in selections:
+            order.append(i)
+    # print('here')
+    # print(order)
+    fig = px.treemap(df_input, path=order, values='Killed')
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 40, 'r': 40}, hovermode='closest')
+    return fig
+
+
+@app.callback(
+    Output('graph_1', 'figure'),
+    Output('slider-text', 'children'),
+    Output('graph_2', 'figure'),
+    Input('year-range-slider', 'value')
+    )
+def update_graph_set_1(year_value):
+    min_year, max_year = year_value
+
+    # Graph 1
+    group_by = ['Year']
+    agg_on = {'eventid': ['size'], 'Killed': ['sum'], 'Wounded': ['sum']}
+    df_kill_wound = df.groupby(group_by).agg(agg_on).reset_index()
+    df_kill_wound.columns = ['Year', 'Attacks', 'Killed', 'Wounded']
+    dff_kill_wound = df_kill_wound.query('Year>={}&Year<={}'.format(min_year, max_year))
+
+    # Graph 2
+    group_by = ['Year', 'Attack Type']
+    agg_on = {'eventid': ['size'], 'Killed': ['sum'], 'Wounded': ['sum']}
+    df_year_attack = df.groupby(group_by).agg(agg_on).reset_index()
+    df_year_attack.columns = ['Year', 'Attack Type', 'Attack', 'Killed', 'Wounded']
+    dff_year_attack = df_year_attack.query('Year>={}&Year<={}'.format(min_year, max_year))
+
+    return update_graph_line(dff_kill_wound), \
+           update_range_slider_text(min_year, max_year), \
+           update_graph_scatter(dff_year_attack)
+
+
+@app.callback(
+    Output('graph_3', 'figure'),
+    Input('year-range-slider', 'value'),
+    Input('dropdown_1', 'value'),
+    Input('dropdown_2', 'value')
+    )
+def update_graph_set_2(year_value, country, selections):
+    min_year, max_year = year_value
+
+    # Graph 3
+    group_by = ['Year', 'Region', 'Country', 'City', 'Attack Type', 'Weapon Type']
+    agg_on = {'eventid': ['size'], 'Killed': ['sum'], 'Wounded': ['sum']}
+    df_country_attack = df.groupby(group_by).agg(agg_on).reset_index()
+    df_country_attack.columns = ['Year', 'Region', 'Country', 'City', 'Attack Type', 'Weapon Type', 'Attack', 'Killed', 'Wounded']
+    dff_country = df_country_attack.query('Year>={}&Year<={}&Country=="{}"'.format(min_year, max_year, country))
+
+    return update_graph_treemap(dff_country, selections)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
